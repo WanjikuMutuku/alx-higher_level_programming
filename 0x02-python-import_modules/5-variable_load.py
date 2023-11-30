423cc0e385c80f36f0228d5ea20485800c283c00
wanjikumutuku <moreenkinyua4@gmail.com>
2023-11-30 05:07:19 -0800

    ./5-variable_load.py

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
-import importlib
-
-if __name__ != "__main__":
-    exit()
-# Import the module
-variable_load_5 = importlib.import_module('variable_load_5')
-
-# Access and print the variable a
-print(variable_load_5.a)
+if __name__ == "__main__":
+    import variable_load_5
+    print(variable_load_5.a)
