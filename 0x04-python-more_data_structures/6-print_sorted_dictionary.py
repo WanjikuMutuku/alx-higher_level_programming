4c8c2ab271735a11811b3ec075caae14797b4729
wanjikumutuku <moreenkinyua4@gmail.com>
2023-12-06 10:46:41 -0800

    6 and 7

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    # Sort the keys alphabetically
+    sorted_keys = sorted(a_dictionary.keys())
+
+    # Iterate through the sorted keys and print key-value pairs
+    for key in sorted_keys:
+        print(f'{key}: {a_dictionary[key]}')
