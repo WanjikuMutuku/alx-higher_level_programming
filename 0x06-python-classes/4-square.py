3f48b9c1e93a6fcf18f53f522294f7bbd43da706
wanjikumutuku <moreenkinyua4@gmail.com>
2023-12-19 09:55:19 -0800

    4-square.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""defines a square"""
+
+
+class Square:
+    """based on 3-square.py"""
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """returns area of a square"""
+        return self.__size ** 2
