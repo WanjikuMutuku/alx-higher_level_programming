85fe1b5b3db9f6ce1fc5284a250ecf16983cbd79
wanjikumutuku <moreenkinyua4@gmail.com>
2023-12-19 10:40:49 -0800

    102-square.py

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""defines a square"""
+
+
+class Square:
+    """based on 4-square.py"""
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """retrieve private instance attribute size"""
+        if not isinstance(value, (int, float)):
+            raise TypeError("size must be a number")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """returns the current square area"""
+        return self.__size ** 2
+
+    def __eq__(self, other):
+        if isinstance(other, Square):
+            return self.area() == other.area()
+        return False
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __lt__(self, other):
+        if isinstance(other, Square):
+            return self.area() < other.area()
+        return False
+
+    def __le__(self, other):
+        if isinstance(other, Square):
+            return self.area() <= other.area()
+        return False
+
+    def __gt__(self, other):
+        if isinstance(other, Square):
+            return self.area() > other.area()
+        return False
+
+    def __ge__(self, other):
+        if isinstance(other, Square):
+            return self.area() >= other.area()
+        return False
