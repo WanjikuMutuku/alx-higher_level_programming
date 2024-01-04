c0a7d104fdb8bca9910a4f9b07b4e9333d3d6894
wanjikumutuku <moreenkinyua4@gmail.com>
2024-01-04 11:38:34 -0800

    1 correction

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python3
+""" 1-rectangle.py """
+
 
 class Rectangle:
+    """ private instance width and height are created """
+
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
 
     @property
+    """ retrives the width """
     def width(self):
         return self.__width
 
     @width.setter
+    """defines the exception conditions for the value of the width """
     def width(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
@@ -18,10 +24,12 @@ class Rectangle:
         self.__width = value
 
     @property
+    """ retrives the height """
     def height(self):
         return self.__height
 
     @height.setter
+    """ determines the exceptions for the height value """
     def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
