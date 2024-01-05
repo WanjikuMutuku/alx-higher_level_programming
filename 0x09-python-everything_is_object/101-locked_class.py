d9728497604d2505ee1e3d21bc8b2b0580172b6b
wanjikumutuku <moreenkinyua4@gmail.com>
2024-01-05 07:47:12 -0800

    101 and 100

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,8 +1,7 @@
 class LockedClass:
     __slots__ = ['first_name']
 
-    def __setattr__(self, name, value):
-        if name == 'first_name':
-            object.__setattr__(self, name, value)
-        else:
-            raise AttributeError("'LockedClass' object has no attribute '{}'".format(name))
+    def __init__(self):
+        """Creates new instances of Locked Class."""
+
+        self.first_name = "first_name"
