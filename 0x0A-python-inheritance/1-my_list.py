19e54771a575786461fac916c29666b331dfe4e5
wanjikumutuku <moreenkinyua4@gmail.com>
2024-01-08 06:36:28 -0800

    0 and 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
+"""
+Module with class MyList
+"""
+
 
 class MyList(list):
+    """ class with method print_sorted """
+    pass
+
     def print_sorted(self):
-        """
-        prints the list, but sorted (ascending sort)
-        """
+        """ prints the list, but sorted (ascending sort) """
 
         sorted_list = sorted(self)
         print(sorted_list)
