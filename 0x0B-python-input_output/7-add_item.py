e532ef949a5976a4c538a6c7a2aa5fec9eb8b107
wanjikumutuku <moreenkinyua4@gmail.com>
2024-01-09 09:53:29 -0800

    7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,39 +1,21 @@
+#!/usr/bin/python3
+"""module with class that add items to a lst """
+
+import json
 import sys
-from os import path
-from typing import List
-from save_to_json_file import save_to_json_file
-from load_from_json_file import load_from_json_file
+import os.path
+from sys import argv
 
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-def add_items_to_list(items: List[str]):
-    """
-    Adds items to a Python list and saves it to a file in JSON format.
+filename = "add_item.json"
+json_list = []
 
-    Args:
-        items (List[str]): The items to add to the list.
+if os.path.exists(filename):
+    json_list = load_from_json_file(filename)
 
-    Returns:
-        None
-    """
-    filename = "add_item.json"
+for index in argv[1:]:
+    json_list.append(index)
 
-    # Check if the file exists
-    if path.exists(filename):
-        # Load the existing list from the file
-        my_list = load_from_json_file(filename)
-    else:
-        # Create an empty list if the file doesn't exist
-        my_list = []
-
-    # Add the items to the list
-    my_list.extend(items)
-
-    # Save the updated list to the file
-    save_to_json_file(my_list, filename)
-
-
-# Get the command-line arguments (excluding the script name)
-arguments = sys.argv[1:]
-
-# Add the arguments to the list
-add_items_to_list(arguments)
+save_to_json_file(json_list, filename)
