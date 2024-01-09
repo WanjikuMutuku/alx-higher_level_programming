a6261aaf7121b4e9c3aca4df9df1e736e6bc728f
wanjikumutuku <moreenkinyua4@gmail.com>
2024-01-09 10:18:11 -0800

    8

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -5,12 +5,4 @@
 def class_to_json(obj):
     """ returns the dictionary description with simple data structure
     for JSON serialization of an object """
-    attributes = obj.__dict__.copy()
-
-    # Handle attributes with name mangling (for private attributes)
-    for key, value in attributes.items():
-        if key.startswith('_'):
-            new_key = '{}{}'.format(obj.__class__.__name__, key)
-            attributes[new_key] = attributes.pop(key)
-
-    return attributes
+    return vars(obj)
