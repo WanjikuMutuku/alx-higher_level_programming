bb5e0245aca9251832db9369e5c9e17fdc2e2aa4
wanjikumutuku <moreenkinyua4@gmail.com>
2024-02-13 13:18:40 -0800

    12-15

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,5 @@
+-- updates the score of Bob to 10 in the table second_table
+-- only the name field and database name will be passed as an argument
+UPDATE second_table
+SET score = 10
+WHERE name = "Bob";
