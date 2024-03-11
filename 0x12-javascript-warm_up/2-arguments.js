755d9292191701b5d8ce7b4c3f6b3a2d825f1b77
WanjikuMutuku <moreenkinyua4@gmail.com>
2024-03-11 11:14:13 -0700

    2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,9 +2,9 @@
 
 const argscount = process.argv.length - 2;
 if (argscount === 0) {
-	console.log('No argument');
+  console.log('No argument');
 } else if (argscount === 1) {
-	console.log('Argument found');
+  console.log('Argument found');
 } else {
-	console.log('Arguments found');
+  console.log('Arguments found');
 }
