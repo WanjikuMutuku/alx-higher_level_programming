bbfefd7c5f1eda1931e0e3e29f927ab80af5e6bf
wanjikumutuku <moreenkinyua4@gmail.com>
2024-03-19 07:34:14 -0700

    9-model_state_filter_a.py

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-prints the first State object from the database hbtn_0e_6_usa
+ lists all State objects that contain the letter a
+ from the database hbtn_0e_6_usa
 """
 import sys
 from sqlalchemy import create_engine
