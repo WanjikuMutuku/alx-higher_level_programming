ba81e09a56294e73616a0648fc9678a5b23deaa8
wanjikumutuku <moreenkinyua4@gmail.com>
2024-03-27 12:34:45 -0700

    4

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL,sends a GET request, and displays the body of the response
-curl -sX GET $1 -H "X-HolbertonSchool-User-Id: 98" -L
+curl -sX GET $1 -H "X-School-User-Id: 98" -L
