b4edc15bc138259c973d7a3bf01d710f62af0087
wanjikumutuku <moreenkinyua4@gmail.com>
2024-03-27 12:31:55 -0700

    5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL, sends a POST request, and displays body of the response
-curl -sX POST $1 -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" -L
+curl -sX POST -d "email=test@gmail.com" -d "subject=I will always be here for PLD" "$1"
