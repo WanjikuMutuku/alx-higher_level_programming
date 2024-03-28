8263e39dae928b089ffb67ac132154eca0d16c00
wanjikumutuku <moreenkinyua4@gmail.com>
2024-03-28 07:20:23 -0700

    10-my_github.py

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" takes your GitHub credentials (username and password) """
+import requests
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+
+    url = "https://api.github.com/user"
+    response = requests.get(url, auth=(username, password))
+
+    if response.status_code == 200:
+        user_data = response.json()
+        user_id = user_data.get('id')
+        print(user_id)
+    else:
+        print("None")
