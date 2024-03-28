acee8b22f1beb33b7f3aba70b628d847db153f69
wanjikumutuku <moreenkinyua4@gmail.com>
2024-03-28 07:31:16 -0700

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,11 +2,14 @@
 """ fetches https://alx-intranet.hbtn.io/status"""
 import urllib.request
 
-if __name__ == "__main__":
-    request = urllib.request.Request("https://intranet.hbtn.io/status")
-    with urllib.request.urlopen(request) as response:
-        body = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode("utf-8")))
+url = "https://alx-intranet.hbtn.io/status"
+
+with urllib.request.urlopen(url) as response:
+    body = response.read()
+    content_type = response.info().get_content_type()
+    utf8_content = body.decode('utf-8')
+
+print("Body response:")
+print(f"    - type: {type(body)}")
+print(f"    - content: {body}")
+print(f"    - utf8 content: {utf8_content}")
